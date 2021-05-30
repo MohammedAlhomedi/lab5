dcb282187903a5354055780407d34636b762e41b
mohahmed2 <>
2021-05-30 20:27:02 +0000

    do lab5

---
+++ app.js
@@ -0,0 +1,36 @@
+
+ var userName = prompt("Whats your name ?");
+alert("welcome to our websit " + userName )
+
+
+//var img= '' ;
+var favcar = prompt("What is your fav car volvo or toyota ?");
+if(favcar === "volvo" ){
+//   // console.log("what do you work ?");
+
+
+//document.write('<img src="https://www.volvocars.com/images/v/intl/v/-/media/project/contentplatform/data/media/my22/xc60-fuel/xc60-gallery-1-1x1.jpg?h=1080&iar=0&w=1080" width="250px" alt="AOT"/>')
+
+
+}else if(favcar === "toyota"){
+//document.write('<img src="https://global.toyota/pages/news/images/2019/11/05/1100/20191105_02_01_s.jpg" width="150px" alt="AOT"/>')
+}else{ alert('please next time answer either volvo or toyota ')}
+
+var car = prompt('how many pictures of cars did you like ? ');
+// var result = 
+ 
+if (car >= 5 ){car=5}
+for(var i =0; i < car ;i++){result =0; 
+result += document.write('<img src="https://cars.usnews.com/images/article/202002/128389/1_title_2020_kia_optima.jpg" width="150" height="160"/>')
+}
+
+
+//if (rate>= i ){rate=5}
+//var rate = ['*', '**', '***', '****', '*****',];
+//for(var i = 0; i < rate.length; i++) {
+//  document.write(rate[i]);
+//}
+
+//var rate = prompt(' rate please ')
+  //console.log(rating[i]);}
+
